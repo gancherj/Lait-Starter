b4cd8a8d2ac83ce5f57552bc6484de64994f574f
gancherj <j.gancher@northeastern.edu>
2026-08-26 10:45:30 -0400

    Update lait

--- LaitStarter/Basic.lean
+++ LaitStarter/Basic.lean
@@ -1,12 +1,12 @@
 import Lait
 #lait
 
-def myList = [1, 2, 3]
+def myList := [1, 2, 3]
 
-def printList xs =
+def printList xs :=
   match xs with
   | [] => ()
-  | x :: xs => (let _ = print (Str.of_num x) in printList xs)
+  | x :: xs => (let _ := print (toString x) in printList xs)
   end
 
 #eval printList myList
--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
 import LaitStarter
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+def main : IO _root_.Unit :=
+  IO.println s!"Hello, world!"
